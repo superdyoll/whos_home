8b4a15152ac9164f03602cca5fc6e4bfe375abe7
Lloyd Pearson <superdyoll@gmail.com>
2020-05-13 20:28:55 +0100

    First attempt at magic mirror module

--- node_helper.js
+++ node_helper.js
@@ -1,6 +1,59 @@
 var NodeHelper = require("node_helper");
+var sqlite3 = require('sqlite3').verbose();
+
 module.exports = NodeHelper.create({
-	start() {
+	config: {},
+
+	updateTimer: null,
+	updateProcessStarted: false,
+
+	start: function () {
 		console.log(`Starting module helper: ${this.name}`);
-	}
+	},
+
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "CONFIG") {
+			console.log("Configuring whos home");
+			this.config = payload;
+			this.performFetch();
+		}
+	},
+
+	performFetch: function() {
+		console.log('Whos home fetch');
+		var self = this;
+		let db = new sqlite3.Database(this.config.database, (err) => {
+			if (err) {
+				console.log(err.message);
+			}
+			console.log('Connected to the whos home database');
+		});
+
+		db.all(`SELECT t1.*, names.name 
+			FROM history t1 
+			LEFT JOIN names
+			ON t1.mac = names.mac`, [],(err, rows) => {
+				if (err) {
+					console.error(err.message);
+				}
+				self.sendSocketNotification("STATUS",rows); 
+			});
+
+		db.close();
+
+		this.scheduleNextFetch(this.config.updateInterval);
+	},
+
+	scheduleNextFetch: function(delay) {
+		if (delay < 60 * 1000) {
+			delay = 60 * 1000;
+		}
+
+		var self = this;
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			self.performFetch();
+		}, delay);
+	},
 });
--- whos_home.js
+++ whos_home.js
@@ -1,3 +1,44 @@
-Module.register("whos_home", {
+var prettydate = require("pretty-date");
 
+Module.register("whos_home", {
+	defaults: {
+		updateInterval: 1 * 60 * 1000, // every 1 minute
+		database: "./whos_home.db"
+	},
+
+	named_devices: null,
+
+	start: function () {
+		var self = this;
+		Log.log("Start Who's Home");
+		setInterval( () => { self.updateDom(2); } , self.config.updateInterval);
+	},
+
+	notificationReceived: function (notification, payload, sender) {
+		if (notification === "DOM_OBJECTS_CREATED") {
+			this.sendSocketNotification("CONFIG", this.config);
+		}
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "STATUS") {
+			this.named_devices = payload;
+			self.updateDom(2);
+		}
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		if (this.named_devices){
+			this.named_devices.forEach((row) => {
+				console.log(row.name);
+				var device = document.createElement("p");
+				device.className = "small bright";
+				device.innerHTML(row.name + " Last seen " + prettydate.format(new Date(row.unixdate * 1000)));
+				wrapper.appendChild(device);
+			});
+		}
+		return wrapper;
+	}
 });
